098dc09af2b9dec602666ffc5943a8f97a561b3b
Jess Pomfret <jesspomfret@datamasterminds.io>
2022-08-21 18:04:11 +0100

    adding action

---
+++ Set-IssueList.ps1
@@ -0,0 +1,27 @@
+<#
+    .SYNOPSIS
+    This script sets the issue list on our readme
+
+    .DESCRIPTION
+    Gets a list of issues in a GitHub repo and formats them into a table within an MD file.
+
+    .NOTES
+    Jess Pomfret
+    @jpomfret
+    #>
+    #param(
+    #    [parameter(Mandatory = $true)]
+    #    [int]$Length
+    #)
+
+$mdOutput = @"
+## Issue List Title
+
+Issue   | Status | Labels
+--------|--------|---------
+ Issue1 | Closed | house
+ Issue2 | Open   | work
+ Issue3 | Open   | personal, bigone
+"@
+
+echo “::set-output name=mdOutput::$mdOutput"
